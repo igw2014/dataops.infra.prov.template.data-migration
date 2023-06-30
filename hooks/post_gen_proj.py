98eb86c73f53643e9ee1f26be3264a1d1960e19f
akstig <tigga.akshay@gmail.com>
2023-06-30 06:06:02 +0530

    removed extra file

--- hooks/post_gen_proj.py
+++
@@ -1,9 +0,0 @@
-import sys, os, shutil
-
-
-def main():
-    proj_dir = "{{ cookiecutter.project_name }}".lower()
-    os.chdir(proj_dir)
-    print(os.getcwd())
-if __name__ == '__main__':
-      sys.exit(main())
