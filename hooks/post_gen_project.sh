aea3dd92d54cffeb8224da85defed0a6aeb2722f
akstig <tigga.akshay@gmail.com>
2023-07-17 14:25:42 +0530

    automated s3 bucket creation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,3 +6,6 @@ cdktf get
 #pipenv shell
 # cdktf deploy s3tfstatestack
 pip install --target=/opt/homebrew/lib/python3.11/site-packages -r requirements.txt
+aws s3api create-bucket --bucket {{ cookiecutter.tenant_name }}-{{ cookiecutter.environment }}-dops-tf-state --region {{ cookiecutter.region }}
+aws s3api put-object --bucket {{ cookiecutter.tenant_name }}-{{ cookiecutter.environment }}-dops-tf-state --key sample-database/
+aws s3api put-object --bucket {{ cookiecutter.tenant_name }}-{{ cookiecutter.environment }}-dops-tf-state --key data-migration/
